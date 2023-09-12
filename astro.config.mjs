b8a8b8c9a097b47d92353170d0cace4c3833b76b
isjdev <isj.code@gmail.com>
2023-09-12 15:53:37 +0200

    sd

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://Isj-Code.github.io',
-  base: '/iberoweb',
+  // site: 'https://Isj-Code.github.io',
+  // base: '/iberoweb',
+  output: "server",
+  adapter: vercel()
 });
